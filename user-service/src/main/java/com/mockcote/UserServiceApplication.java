1d5bf57a27c518d4fd90c62485ddfca6d4ca9f46
백승훈 <khbak111@naver.com>
2024-12-19 13:23:03 +0900

    user start

--- user-service/src/main/java/com/mockcote/UserServiceApplication.java
+++ user-service/src/main/java/com/mockcote/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.user;
+package com.mockcote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ user-service/src/main/java/com/mockcote/user/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.mockcote.user.controller;
+
+public class UserController {
+
+}
